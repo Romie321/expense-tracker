4ba6c00f81da669a1382bc21de355a957c7fa39b
Romie321 <raguilera321@gmail.com>
2026-08-01 19:33:56 -0700

    fix: add expense amount element to script for functionality

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const balanceEl = document.getElementById("balance");
 const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
